d1fd62acca83d03613db6173bb07b74bc5ca1ae1
Meron Habtemichael <meronmkl@gmail.com>
2021-12-06 08:38:56 +0100

    xc

--- Josephus_problem.py
+++ Josephus_problem.py
@@ -1,30 +1,30 @@
 """
-Custom singly LinkedList - AddressBookPage
-Each node containing - name, address and a point to the next node if any
-
-The Tail of the LinkedList points to none
-And the Head is the fist element
+Josephus problem
 """
+
+import collections
+
+
+# --------------------------------------- LinkedList without Iterator -------------------------------------------
 
 
 class Node:
-    address = None
-    name = None
+    data = None
     next_node = None
 
-    def __init__(self, name, address):
+    def __init__(self, data):
         """Create an instance node"""
-        self.name = name
-        self.address = address
+        self.data = data
 
     def __repr__(self):
         """Represent a node"""
-        return '[%s: %s]' % (self.name, self.address)
+        return '[%s]' % self.data
 
 
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
         self.length = 0
 
     def is_empty(self):
@@ -35,12 +35,18 @@ class LinkedList:
         """Return the size of the linked list"""
         return self.length
 
-    def add(self, name, address):
+    def add(self, data):
         """Add note in front of the header, simple implementation"""
         self.length += 1
-        new_node = Node(name, address)  # create the node to implement the node
-        new_node.next_node = self.head  # reference the next node to the current head
-        self.head = new_node  # Add at the first of the linked list
+        new_node = Node(data)
+        if self.length != 1:
+            self.tail.next_node = new_node
+            new_node.next_node = None
+            self.tail = new_node
+        else:
+            new_node.next_node = self.head  # reference the next node to the current head
+            self.head = new_node  # Add at the first of the linked list
+            self.tail = new_node
 
     def __repr__(self):
         """Do the representation fo the Linked List"""
@@ -48,11 +54,11 @@ class LinkedList:
         i = self.head
         while i:
             if i is self.head:
-                elements.append('☺ [%s: %s]' % (i.name, i.address))
+                elements.append('☺ [%s]' % i.data)
             elif i.next_node is None:
-                elements.append('[%s: %s] ✌ ' % (i.name, i.address))
+                elements.append('[%s] ✌ ' % i.data)
             else:
-                elements.append('[%s: %s]' % (i.name, i.address))
+                elements.append('[%s]' % i.data)
             i = i.next_node
         return '➤'.join(elements)
 
@@ -67,16 +73,7 @@ class LinkedList:
         while position < node_index:
             i = i.next_node
             position += 1
-        return i  # found and return the element
-
-    # add functionality of iterables to iterate through the list
-    # the iterator includes next(), previous(), hasNext(), and the most important - Remove()
-    # Include a midCount/check the book/ to indicate new modifications after an iterator is created. page 76 of the book
-    # keep truck of size value to operate in constant time ie replace the size function
-    # here is a resource https://www.youtube.com/watch?v=jTYiNjvnHZY
-    # Add clear method for node to clear its contents
-    # check the iterator from the book. Helpful
-    # The remove operation is nasty - changing mid-count
+        return i.data  # found and return the element
 
     def remove(self, node_index):
         """Remove node from LinkedList using index"""
@@ -98,19 +95,57 @@ class LinkedList:
         return "Removed : %s" % tobe_removed
 
 
-l = LinkedList()
+# --------------------------------------------------------------------------------------------------------------
 
-meron = Node('Mike', 'Stockholm')
-l.head = meron
-l.add('Yonatan', 'Asmara')
-l.add('Teddy', 'Kristianstad city')
-l.add('John', 'Morocco')
-l.add('Edward', 'Spain')
-print('The size is: %s' % l.size())
-print(l.get(3))
-print(l)
-print(l.remove(4))
-print(l)
-print('The size is: %s' % l.size())
-print('---------------')
-print(format(l))
+
+n = 5
+m = 2
+
+numbers = []
+myLinkedList = LinkedList()
+linked_lst = collections.deque()
+for e in range(1, n + 1):
+    myLinkedList.add(e)
+    numbers.append(e)
+    linked_lst.append(e)
+
+
+def josephus(ls, skip, data_structure):
+
+    if data_structure == "arrayList":
+        skip -= 1  # pop automatically skips the dead guy
+        idx = skip
+        while len(ls) > 1:
+            print(ls.pop(idx))  # kill prisoner at idx
+            idx = (idx + skip) % len(ls)
+        print('survivor: ', ls[0])
+    if data_structure == "arrayList":
+        pass
+
+
+josephus(numbers, m, 'hey')
+
+
+def josephus(people, skips):
+    if people == 1:
+        return 1
+    else:
+
+        # The position returned by
+        # josephus(n - 1, k) is adjusted
+        # because the recursive call
+        # josephus(n - 1, k) considers
+        # the original position
+        # k%n + 1 as position 1
+        return (josephus(people - 1, skips) + skips - 1) % people + 1
+
+
+# print(numbers)
+
+n = 5
+k = 2
+
+print("The chosen place is ", josephus(n, k))
+
+# print(myLinkedList.get(7))
+# # print(linked_lst)
